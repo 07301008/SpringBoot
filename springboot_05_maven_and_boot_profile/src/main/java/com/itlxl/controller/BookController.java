cab796c760b2c811234f6fbd1bd6db231060d7da
itlxl <1763308858@qq.com>
2022-09-02 21:06:25 +0800

    初始化SpringBoot

---
+++ springboot_05_maven_and_boot_profile/src/main/java/com/itlxl/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.itlxl.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    @RequestMapping("/{id}")
+    public String getById(@PathVariable Integer id){
+        System.out.println("id ==> " + id);
+        return "hello , spring boot";
+    }
+}
---
+++ springboot_07_test/src/test/java/com/itlxl/Springboot07TestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itlxl;
+
+import com.itlxl.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot07TestApplicationTests {
+    @Autowired
+    private BookService bookService;
+    @Test
+    public void save() {
+        bookService.save();
+    }
+}
